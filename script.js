36d151d7410b0ea07d0be0f2389f77f59811430c
Kaleb-07 <kalebmulugeta9519@gmail.com>
2025-07-03 15:50:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -373,4 +373,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const target = document.querySelector(this.getAttribute("href"))
 
-
+      if (target) {
+        window.scrollTo({
+          top: target.offsetTop,
+          behavior: "smooth",
+        })
+      }
+    })
+  })
+  // Initialize testimonials
+  showTestimonial(0)
+})
